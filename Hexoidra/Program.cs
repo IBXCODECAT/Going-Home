77852ed9f2d8a0b371ee9a0d52bd6ad73732a306
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-10-19 10:50:25 -0500

    Create Game Window

---
+++ Hexoidra/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Hexoidra
+{
+    class Program
+    {
+        private static void Main(String[] args)
+        {
+            using(Game game = new Game(500, 500))
+            {
+                game.Run();
+            }
+        }
+    }
+}
